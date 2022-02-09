1a8e85c481fcea699af4a2140fdccb691c0f81e1
Tannon Kew <kew@cl.uzh.ch>
2022-02-09 17:33:02 +0100

    include bertscore and QE

--- simplification_evaluation.py
+++ simplification_evaluation.py
@@ -9,16 +9,18 @@ Example call:
         --ref_file \
         --hyp_file 
 
-    python simple_eval.py \
+    python simplification_evaluation.py \
         --src_file /srv/scratch6/kew/ats/data/en/aligned/turk_test.tsv \
         --hyp_file /srv/scratch6/kew/ats/fudge/results/bart_large_paraNMT_filt_fr/turk_test/lambda0.0_pretopk200_beams4_estopFalse_maxl128_minl1_sampleFalse_lp1.0_norep1_bgrps1_nbest1_repp1.0_softFalse_temp1.0_topk0_topp1.0.txt
 
 """
 
-import argparse
-# import easse
-from easse import sari, bleu, fkgl # samsa fails dep: tupa
 
+import argparse
+
+import torch # for bertscore
+from easse import sari, bleu, fkgl, bertscore, quality_estimation # samsa fails dep: tupa
+ 
 def set_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--src_file', type=str, required=True, help='')
@@ -42,27 +44,6 @@ def read_split_lines(filename, split_sep='<::::>'):
 
     return split_instances
 
-# def get_lines(file, contains_refs=False):
-#     """
-#     if contains_refs=True, expects a tsv format, with src
-#     texts in column 1 and all available references in
-#     subsequent columns, e.g. TURK has 8 simple references per
-#     complex sentence.
-#     """
-#     all_texts, more_texts = [], []
-#     with open(file, 'r', encoding='utf8') as f:
-#         for line in f:
-#             if contains_refs:
-#                 texts = line.strip().split('\t')
-#                 src = texts[0]
-#                 ref = texts[1:] # all colums after col 1
-#                 all_texts.append(src)   
-#                 more_texts.append(ref)
-#             else:
-#                 all_texts.append(line.strip())
-
-#     return all_texts, more_texts
-
 if __name__ == '__main__':
 
     args = set_args()
@@ -80,8 +61,21 @@ if __name__ == '__main__':
     assert len(hyp_sents) == len(src_sents)
 
     results = {'file': args.hyp_file}
+    
+
+    # breakpoint()
+    # if torch.cuda.is_available():
+    #     precision, recall, f1 = bertscore.corpus_bertscore(hyp_sents, refs_sents)
+    # else:
+    precision, recall, f1 = None, None, None
+
     results['bleu'] = bleu.corpus_bleu(hyp_sents, refs_sents)
     results['sari'] = sari.corpus_sari(src_sents, hyp_sents, refs_sents, legacy=False)
     results['fkgl'] = fkgl.corpus_fkgl(hyp_sents)
-
+    results['bertscore_p'] = precision * 100 if precision else None
+    results['bertscore_r'] = recall * 100 if recall else None
+    results['bertscore_f1'] = f1 * 100 if f1 else None
+    qe = quality_estimation.corpus_quality_estimation(src_sents, hyp_sents)
+    results.update(qe)
+    
     print(results)
